02cc9438cefc41f017081559a05cbc7fbe2c27de
Vincent Tang <vincenttang@Vincents-MacBook-Pro.local>
2018-11-17 14:57:30 -0500

    Added ScreenHome Wrapper

---
+++ src/components/ScreenHome.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ScreenHome extends React.Component {
+  static propTypes = {};
+
+  state = {};
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    // alignItems: 'center',
+    flex: 1,
+  },
+};
+
+export default ScreenHome;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,14 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import ScreenWrapper from '../components/ScreenWrapper';
+import ScreenHome from '../components/ScreenHome';
 import logo from '../images/logo.png';
 
-// import React from 'react';
+// Screen Appbar Imports
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -20,7 +18,7 @@ class Home extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <ScreenWrapper>
+      <ScreenHome>
         <div className={classes.root}>
           <AppBar position="static">
             <Toolbar>
@@ -41,35 +39,10 @@ class Home extends React.Component {
         <Button variant="text" component={Link} to="/quiz">
           Continue
         </Button>
-      </ScreenWrapper>
+      </ScreenHome>
     );
   }
 }
-
-// function ButtonAppBar(props) {
-//   const { classes } = props;
-//   return (
-//     <div className={classes.root}>
-//       <AppBar position="static">
-//         <Toolbar>
-//           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography variant="h6" color="inherit" className={classes.grow}>
-//             News
-//           </Typography>
-//           <Button color="inherit">Login</Button>
-//         </Toolbar>
-//       </AppBar>
-//     </div>
-//   );
-// }
-
-// ButtonAppBar.propTypes = {
-//   classes: PropTypes.object.isRequired,
-// };
-
-// export default withStyles(styles)(ButtonAppBar);
 
 const styles = {
   container: {
