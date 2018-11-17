8372e47fd00e5d2d5f9362c2f2c23496d178ed42
Vincent Tang <vincenttang@Vincents-MacBook-Pro.local>
2018-11-17 14:48:36 -0500

    working material ui implementation but still ugly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,35 @@ import { Link } from 'react-router-dom';
 import ScreenWrapper from '../components/ScreenWrapper';
 import logo from '../images/logo.png';
 
+// import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 class Home extends React.Component {
+  
   render() {
+    const { classes } = this.props;
     return (
       <ScreenWrapper>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" className={classes.grow}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
         <Typography noWrap variant={'h1'}>
           Welcome
         </Typography>
@@ -21,10 +46,45 @@ class Home extends React.Component {
   }
 }
 
+// function ButtonAppBar(props) {
+//   const { classes } = props;
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="static">
+//         <Toolbar>
+//           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography variant="h6" color="inherit" className={classes.grow}>
+//             News
+//           </Typography>
+//           <Button color="inherit">Login</Button>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
+
+// ButtonAppBar.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+
+// export default withStyles(styles)(ButtonAppBar);
+
 const styles = {
   container: {
     flexDirection: 'column',
   },
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
 };
 
-export default Home;
+export default withStyles(styles)(Home);
