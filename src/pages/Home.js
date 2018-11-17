c5e7829904142ab3ba22fa80b221b9f7e2c10073
Ian Grail <grailians@gmail.com>
2018-11-17 14:08:57 -0500

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import ScreenWrapper from '../components/ScreenWrapper';
 import logo from '../images/logo.png';
 
 class Home extends React.Component {
   render() {
     return (
-      <div style={styles.container}>
+      <ScreenWrapper>
         <Typography noWrap variant={'h1'}>
           Welcome
         </Typography>
@@ -16,7 +16,7 @@ class Home extends React.Component {
         <Button variant="text" component={Link} to="/quiz">
           Continue
         </Button>
-      </div>
+      </ScreenWrapper>
     );
   }
 }
