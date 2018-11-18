82fc6a3143d2329a52e9ad65717e223506b2c7e7
Ian Grail <grailians@gmail.com>
2018-11-17 19:20:22 -0500

    create result screen after each answer submission

---
+++ src/components/SingleResult.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Check from '@material-ui/icons/Check';
+import Close from '@material-ui/icons/Close';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+class SingleResult extends React.PureComponent {
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    isCorrect: PropTypes.bool.isRequired,
+  };
+
+  render() {
+    return (
+      <div
+        style={{ ...styles.container, background: this.props.color }}
+        onClick={this.onClick}
+      >
+        {this.props.isCorrect ? <Check /> : <Close />}
+        <Typography variant="title">
+          {this.props.isCorrect ? 'Correct.' : 'Incorrect.'}
+        </Typography>
+        <Button
+          variant="contained"
+          fullWidth
+          onClick={this.props.onClick}
+        >
+          {this.props.isCorrect ? 'Next' : 'Try Again'}
+        </Button>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    width: 300,
+    height: 100,
+    margin: 16,
+  },
+};
+
+export default SingleResult;
+
