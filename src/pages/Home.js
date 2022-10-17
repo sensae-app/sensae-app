a19535c26dadd1459a4c3ed07e3ab5a92ca33830
Ian Grail <grailians@gmail.com>
2022-10-17 12:12:56 -0400

    chore: fix eslint warning

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
